7aea9a31e801bd8cec8419aef9b086316a242180
RamitShapkota <shapkotaramit47@gmail.com>
2026-05-04 20:59:14 +0545

    first backend

--- 02-Deploy-backend-code/index.js
+++ 02-Deploy-backend-code/index.js
@@ -1,10 +1,24 @@
+require('dotenv').config()
+
 const express = require('express')
 // import express from "express" same as above line
 const app = express()
-const port = 3000
+const port =process.env.PORT || 300
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/twitter', (req,res) => {
+  res.send('RamitSonar')
+})
+
+app.get('/login',(req,res) => {
+  res.send('<h1>This is your login page</h1>')
+})
+
+app.get('/youtube', (req,res) => {
+  res.send('<h2>This is youtube page</h2>')
 })
 
 app.listen(port, () => {
