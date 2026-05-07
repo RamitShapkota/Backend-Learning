8522a0723893aff08532410d0508213f9276c150
RamitShapkota <shapkotaramit47@gmail.com>
2026-05-07 19:41:49 +0545

    add some frontend and security feature

---
+++ 03-FullStack-Basic/frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import axios from 'axios'
+import { useEffect } from 'react';
+
+function App() {
+ const [jokes, setJoks] = useState([]);
+
+ useEffect(() => {
+
+  //add Proxy to avoid below long url
+  axios.get('http://http://localhost:3000/api/jokes')
+  .then((response)=>{
+    setJoks(response.data);
+  })
+  .catch((error)=>{
+    console.log(error);
+  })
+ })
+
+  return (
+    <>
+      <h1>Full Stack Application</h1>
+      <p>JOKES: {jokes.length}</p>
+
+      {
+        jokes.map((joke,index) => {
+          <div key={joke.id}>
+            <h3>{joke.title}</h3> 
+            <p>{joke.content}</p>
+          </div>
+        })
+      }
+    </>
+  )
+}
+
+export default App
