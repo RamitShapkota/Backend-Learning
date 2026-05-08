f0c79594bb746c67631f686a2959adb1fea10f84
RamitShapkota <shapkotaramit47@gmail.com>
2026-05-08 18:58:28 +0545

    add fallback route

--- 03-FullStack-Basic/backend/index.js
+++ 03-FullStack-Basic/backend/index.js
@@ -1,56 +1,44 @@
 require('dotenv').config();
 
-const express = require("express");  //common js
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(express.static('dist'));  //middleware
 
-// app.get("/" , (req,res) => {
-//     res.send("server is ready");
-// })
+// Serve frontend static files
+app.use(express.static(path.join(__dirname, 'dist')));
 
-//get a list of 5 jokes
 
-app.get("/api/jokes",(req,res)=>{
+// API Route
+app.get("/api/jokes", (req, res) => {
+
     const jokes = [
-  {
-    id: 1,
-    title: 'The Programmer',
-    content: 'Why do programmers prefer dark mode? Because light attracts bugs.'
-  },
-  {
-    id: 2,
-    title: 'The Database',
-    content: 'A SQL query walks into a bar, walks up to two tables, and asks, "Can I join you?"'
-  },
-  {
-    id: 3,
-    title: 'The Keyboard',
-    content: 'Why was the computer cold? It left its Windows open.'
-  },
-  {
-    id: 4,
-    title: 'The Web Developer',
-    content: 'How do you comfort a JavaScript bug? You console it.'
-  },
-  {
-    id: 5,
-    title: 'The Coffee',
-    content: 'Java developers never drink decaf because they can’t handle the lack of "Beans."'
-  },
-  {
-    id: 6,
-    title: 'The Coffee',
-    content: 'Java developers never drink decaf because they can’t handle the lack of "Beans."'
-  }
-];
-res.send(jokes)
+        {
+            id: 1,
+            title: 'The Programmer',
+            content: 'Why do programmers prefer dark mode? Because light attracts bugs.'
+        },
+        {
+            id: 2,
+            title: 'The Database',
+            content: 'A SQL query walks into a bar and asks, "Can I join you?"'
+        }
+    ];
 
-})
+    res.send(jokes);
 
-app.listen(port , () =>{
-    console.log(`Serve at http://localhost:${port}`);
-})
+});
+
+
+// React fallback route
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
