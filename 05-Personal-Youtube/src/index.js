8511097d8a0d42d59fcaa8c6eace35ad2dc334ff
RamitShapkota <shapkotaramit47@gmail.com>
2026-05-11 20:19:12 +0545

    utils code discussion

--- 05-Personal-Youtube/src/index.js
+++ 05-Personal-Youtube/src/index.js
@@ -6,13 +6,26 @@ dotenv.config({
 
 
 import connectDB from './db/index.js';
+import { app } from "./app.js";
+
+const port = process.env.PORT || 8000;
 
 
 
 
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("EXPRESS SERVER ERROR :",error);
+    })
 
-
-connectDB();
+    app.listen(port , () => {
+        console.log(`server is running at port : ${port}`);
+    } )
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ",err);
+})
 
 
 
