d3a6d1de1b541079ac09e74f59cecedca1126674
RamitShapkota <shapkotaramit47@gmail.com>
2026-05-17 15:44:40 +0545

    write controllers , routes logic

--- 05-Personal-Youtube/src/app.js
+++ 05-Personal-Youtube/src/app.js
@@ -20,4 +20,16 @@ app.use(express.json({ limit: "16kb" })); //allow json
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); //for url
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//routes import 
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users" , userRouter)  //mouting  a router with a base path.
+
+
+// http://localhost:8000/api/v1/users/register
+
+
+
 export { app };
