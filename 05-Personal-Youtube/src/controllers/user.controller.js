f8780831973212a52593ee943b6c1d629d013360
RamitShapkota <shapkotaramit47@gmail.com>
2026-05-17 21:47:14 +0545

    testing the API in thunder client

--- 05-Personal-Youtube/src/controllers/user.controller.js
+++ 05-Personal-Youtube/src/controllers/user.controller.js
@@ -3,8 +3,13 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler( async (req, res) => {
     res.status(200).json({
-        message: "ok"
+        message: "register successfully"
+    })
+})
+const loginUser = asyncHandler( async (req, res) => {
+    res.status(200).json({
+        message: "login sucessfully"
     })
 })
 
-export {registerUser}
+export {registerUser , loginUser}
