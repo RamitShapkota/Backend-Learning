fa5af379533819b405a260db72c4c2e5da7ed4db
RamitShapkota <shapkotaramit47@gmail.com>
2026-05-27 22:09:12 +0545

    add routes for changing password, retrieving current user, updating account details, and updating user images

--- 05-Personal-Youtube/src/routes/user.routes.js
+++ 05-Personal-Youtube/src/routes/user.routes.js
@@ -4,6 +4,11 @@ import {
   logoutUser,
   registerUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserCoverImage,
+  updateUserAvatar,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -30,4 +35,19 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+
 export default router;
